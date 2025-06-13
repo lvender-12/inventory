e347ce26ea61a37310afaf5bdeed3d7abfc52a15
Lvender <muhamaddamar1812@gmail.com>
2025-06-13 17:32:46 +0700

    perbaikan insert data

--- controller/productsController.js
+++ controller/productsController.js
@@ -1,6 +1,7 @@
 const productsModel = require('../model/productsModel');
 const categoriesModel = require('../model/categoriesModel');
 const validator = require('validator');
+const mongoose = require('../config/db')
 
 class productsController{
     async GetAllProducts(req,res){
@@ -12,36 +13,47 @@ class productsController{
     async AddProduct(req,res){
         const products = await req.body;
         const err = [];
-        const existing = categoriesModel.findOne(_id = products.categories_id)
+        // const existing = await categoriesModel.findOne({ _id: products.categories });
 
         if(validator.isEmpty(products.name || products.stock || products.price || products.categories_id)){
             err.push({field: 'form', msg : 'tidak boleh kosong selain deskripsi'});
         }
 
-        if(products.categories_id !== existing){
-            err.push({field: 'stock or price', msg : 'kategori tidak ada'});
+        if (!mongoose.Types.ObjectId.isValid(products.categories_id)) {
+            err.push({ field: 'categories_id', msg: 'ID kategori tidak valid' });
+        } else {
+            // Cek apakah ID tersebut ada di koleksi
+            const existing = await categoriesModel.findById(products.categories_id);
+            if (!existing) {
+                err.push({ field: 'categories_id', msg: 'Kategori tidak ditemukan' });
+            }
+            
         }
 
-        if(validator.isNumeric(products.stock || products.price)){
+        
+        if(!validator.isNumeric(products.stock || products.price)){
             err.push({field: 'stock or price', msg : 'stock dan price harus numeric'});
         }
 
-        if(err.length > 0){
-            res.send(404).json(products, err);
+        products.stock = Number(products.stock)
+        products.price = Number(products.price)
+        
+        if (err.length > 0) {
+            return res.status(400).json({ errors: err });
         }
 
         try {
             const newProduct = await productsModel.create({
-                nama,
-                stock,
-                price,
-                category_id,
-                deskripsi,
+                name: products.name,
+                stock : products.stock,
+                price : products.price,
+                categories_id: products.categories_id,
+                deskripsi: products.deskripsi,
             });
 
-            res.status(201).json(newProduct);
+            return res.status(201).json(newProduct);
         } catch (error) {
-            res.status(500).json({ error: 'Gagal menambahkan produk', detail: error.message });
+            return res.status(500).json({ error: 'Gagal menambahkan produk', detail: error.message });
         }
     }
 }
