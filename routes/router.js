ed324f5a60df7f3653da55b3dc139ff3e9cda716
Lvender <muhamaddamar1812@gmail.com>
2025-06-13 16:53:45 +0700

    perbaikan insert product

--- routes/router.js
+++ routes/router.js
@@ -5,6 +5,6 @@ const categoriesController = require('../controller/categoriesController')
 
 router.get('/product', productsController.GetAllProducts);
 router.get('/product/add', categoriesController.GetAllCategories);
-// router.post('/product/add',)
+router.post('/product/add', productsController.AddProduct)
 
 module.exports = router
