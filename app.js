590147dd8884dc13000506abc597aba1d51911e0
Lvender <muhamaddamar1812@gmail.com>
2025-06-15 18:26:24 +0700

    penambahan cors

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 require('dotenv').config();
 const db = require('./config/db.js');
 const routes = require('./routes/router.js');
+const cors = require("cors");
 
 
 const port = process.env.PORT || 3000
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(routes)
 
